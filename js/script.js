b5c25046d444ecfde2d9aec23c88c6251b45be46
Richard You <richardyou94@gmail.com>
2023-01-30 22:55:21 -0800

    creating the selection mode for choosing rock, paper, scissor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-var btnTest = document.querySelector("#test");
+var mainBtn = document.querySelector("#main-btn");
 var mainText = document.querySelector("#main-text");
 var selectBtn = document.querySelector("#select");
+var oppoHands = document.querySelectorAll('.opponent-hand');
+var userHands = document.querySelectorAll('.user-hand');
+var btnHand = document.querySelectorAll('.btn-hand');
 
 mainText.addEventListener("click", () => {
     if (mainText.textContent == "start game") {
@@ -13,5 +16,17 @@ function startGame() {
   setTimeout(() => {
     mainText.textContent = "select";
     mainText.classList.add("show");
-  }, 1000);
+    mainBtn.classList.remove('btn-start')
+  }, 500);
+  oppoHands[0].style.transform = "translate(0%, -60%)";
+  oppoHands[1].style.transform = "translate(0%, -60%)";
+  oppoHands[2].style.transform = "translate(0%, -60%)";
+  userHands[0].style.transform = "translate(0%, 30%)";
+  userHands[1].style.transform = "translate(0%, 30%)";
+  userHands[2].style.transform = "translate(0%, 30%)";
+
+  btnHand[0].style.opacity = "1";
+  btnHand[2].style.opacity = "1";
+  btnHand[1].style.opacity = "1";
+  
 }
